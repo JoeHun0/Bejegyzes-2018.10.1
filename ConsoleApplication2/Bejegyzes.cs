e54606b7bf6aba927569c475866e33bedd655119
Petrik Lajos Szg <info@petrik.hu>
2018-10-01 14:59:43 +0200

    Initial commit

---
+++ ConsoleApplication2/Bejegyzes.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Bejegyzes
+    {
+        private string szerzo;
+        private string tartalom;
+        private int likeok = 0;
+        private DateTime letrejott =  DateTime.Now;
+        private DateTime szerkesztve = DateTime.Now;
+
+        public string Szerzo
+        {
+            get
+            {
+                return szerzo;
+            }
+        }
+
+        public string Tartalom
+        {
+            get
+            {
+                return tartalom;
+            }
+
+            set
+            {
+                tartalom = value;
+                szerkesztve = DateTime.Now;
+            }
+        }
+
+        public DateTime Letrejott
+        {
+            get
+            {
+                return letrejott;
+            }
+        }
+
+        public DateTime Szerkesztve
+        {
+            get
+            {
+                return szerkesztve;
+            }
+
+            
+        }
+        public void like()
+        {
+            likeok++;
+        }
+
+        public Bejegyzes(string szerzo, string tartalom)
+        {
+            this.szerzo = szerzo;
+            this.Tartalom = tartalom;
+        }
+        public string kiir(){
+            return szerzo + " - " + likeok + " - " + letrejott + "\r\n Szerkesztve: " + szerkesztve + "\r\n" + Tartalom ;
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     
+            List<Bejegyzes> list = new List<Bejegyzes>();
+            list.Add(new Bejegyzes ("akárki", "Valami"));
+            list.Add(new Bejegyzes("akárki2", "Valami2"));
+            Console.WriteLine("Kérek egy számot.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num);
+        }
+    }
+}
